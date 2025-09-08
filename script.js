0611ec819a9e6fc18081dd16ea18380c6f03d661
Bruno Fabbio <brunobiodev@gmail.com>
2025-09-08 20:39:01 -0300

    feat: define variáveis para script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+import filmes from "./data_filmes.js";
+
+const listaFilmes = document.getElementById("lista-filmes");
+const minhaListaElemento = document.getElementById("minha-lista");
+const inputBusca = document.getElementById("input-busca");
+const botoesGeneroElement = document.getElementById("botoes-genero");
+const btnOrdenarNota = document.getElementById("ordenar-nota");
+const btnOrdenarAno = document.getElementById("ordenar-ano");
