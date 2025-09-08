b093c627f06084935cafec3c19fc4c792bdf8dec
Bruno Fabbio <brunobiodev@gmail.com>
2025-09-08 20:42:03 -0300

    feat: adiciona função para redenrizar minha lista

--- script.js
+++ script.js
@@ -30,3 +30,36 @@ function renderizarFilmes(listaParaRenderizar) {
     listaFilmes.appendChild(card);
   });
 }
+
+function renderizarMinhaLista() {
+  minhaListaElemento.innerHTML = "";
+
+  minhaLista.forEach((filme) => {
+    const card = document.createElement("div");
+    card.classList.add("card-filme");
+
+    card.innerHTML = `
+      <img src="${filme.posterUrl}" alt="Pôster de ${filme.titulo}">
+      <h2>${filme.titulo} (${filme.ano})</h2>
+      <p>Nota: ${filme.nota}</p>
+      <button>Remover</button>
+    `;
+
+    const btn = card.querySelector("button");
+    btn.addEventListener("click", () => removerMinhaLista(filme.id));
+
+    minhaListaElemento.appendChild(card);
+  });
+}
+
+function adicionarMinhaLista(filme) {
+  if (!minhaLista.some((f) => f.id === filme.id)) {
+    minhaLista.push(filme);
+    renderizarMinhaLista();
+  }
+}
+
+function removerMinhaLista(id) {
+  minhaLista = minhaLista.filter((f) => f.id !== id);
+  renderizarMinhaLista();
+}
