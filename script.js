fc7e8933ce57cb32b5cd1f32c22fc89e65ea7c69
Bruno Fabbio <brunobiodev@gmail.com>
2025-09-08 20:39:58 -0300

    feat: adiciona função para redenrizar filmes

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ const inputBusca = document.getElementById("input-busca");
 const botoesGeneroElement = document.getElementById("botoes-genero");
 const btnOrdenarNota = document.getElementById("ordenar-nota");
 const btnOrdenarAno = document.getElementById("ordenar-ano");
+
+let filmesRenderizados = [...filmes];
+let minhaLista = [];
+
+function renderizarFilmes(listaParaRenderizar) {
+  listaFilmes.innerHTML = "";
+
+  listaParaRenderizar.forEach((filme) => {
+    const card = document.createElement("div");
+    card.classList.add("card-filme");
+
+    card.innerHTML = `
+      <img src="${filme.posterUrl}" alt="Pôster de ${filme.titulo}">
+      <h2>${filme.titulo} (${filme.ano})</h2>
+      <p>Nota: ${filme.nota}</p>
+      <button>Adicionar à Minha Lista</button>
+    `;
+
+    const btn = card.querySelector("button");
+    btn.addEventListener("click", () => adicionarMinhaLista(filme));
+
+    listaFilmes.appendChild(card);
+  });
+}
